a57eebbb3c3f887f4f0447eb11be08f8fab436f2
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2024-08-27 22:52:21 +0300

    update_27/08/2024

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,58 @@
 import scss from './Header.module.scss';
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as Polygon} from "../../images/polygon.svg";
+import { ReactComponent as HandshakeWhite} from "../../images/handshake_white.svg";
 import { IoMdMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
-  const [isActiveMenuLang, setActiveMenuLang] = useState(false);
-  const menuLangRef = useRef(null);
+  const [isActiveMenuLang, setIsActiveMenuLang] = useState(false);
+  const [isActiveMobileMenu, setIsActiveMobileMenu] = useState(false);
 
-  const useClickOutside = (ref, callback) => {
-    const handleClick = (e) => {
-      if(ref.current && !ref.current.contains(e.target)) {
-        callback();
-      }
+  const toggleLangMenu = () => {
+    setIsActiveMenuLang(!isActiveMenuLang);
   };
 
-    useEffect(() => {
-      document.addEventListener("mousedown", handleClick);
-      return () => {
-        document.removeEventListener("mousedown", handleClick)
-      };
-    });
+  const toggleMobileMenu = () => {
+    setIsActiveMobileMenu(!isActiveMobileMenu);
   };
-
-  useClickOutside(menuLangRef, () => {
-    closeMenu();
-  });
-
-  const openMenu = () => {
-    document.body.style.cssText = `overflow-y: hidden`
-    setActiveMenuLang(true);
-  };
-
-  const closeMenu = () => {
-    document.body.style.cssText = `overflow-y: auto`
-    setActiveMenuLang(false);
-  };
-
-  console.log(isActiveMenuLang)
 
   return (
+    <>
     <header className={scss.header_container}>
-        <button className={scss.lang_switcher} onClick={openMenu}>
+        <button className={scss.lang_switcher} onClick={toggleLangMenu}>
           <span className={scss.lang}>UK</span>
-          <Polygon className={scss.polygon}/>
+          <Polygon className={isActiveMenuLang ? (scss.polygon_reverse) : (scss.polygon)}/>
         </button>
-        <button className={scss.menu}><IoMdMenu className={scss.menu_icon}/></button>
+        <button className={scss.menu} onClick={toggleMobileMenu}><IoMdMenu className={scss.menu_icon}/></button>
+        <div className={isActiveMenuLang ? (scss.lang_menu_active) : (scss.lang_menu)}> 
+            <span className={scss.lang}>EN</span>
+            <span className={scss.lang}>DE</span>
+        </div>
     </header>
+    <div className={isActiveMobileMenu ? (scss.mobile_menu_active) : (scss.mobile_menu)}>
+        <div className={scss.mobile_menu_buttons}>
+          <button className={scss.mobile_lang_switcher} onClick={toggleLangMenu}>
+              <span className={scss.lang}>UK</span>
+              <Polygon className={isActiveMenuLang ? (scss.polygon_reverse) : (scss.polygon)}/>
+          </button>
+          <button className={scss.close_menu} onClick={toggleMobileMenu}><IoMdClose className={scss.close_menu_icon}/></button>
+        </div>
+        <nav className={scss.mobile_menu_nav}>
+          <NavLink className={scss.current}>Головна</NavLink>
+          <NavLink className={scss.nav}>Про нас</NavLink>
+          <NavLink className={scss.nav}>Ребілітаційна програма</NavLink>
+          <NavLink className={scss.nav}>Новини</NavLink>
+          <NavLink className={scss.nav}>Відгуки</NavLink>
+          <NavLink className={scss.nav}>Наші партнери</NavLink>
+        </nav>
+        <Link className={scss.button_support}>
+            <HandshakeWhite/>
+            <span>Підтримати проєкт</span>
+        </Link>
+    </div>
+    </>
         
   )
 };
