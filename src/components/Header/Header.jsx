e576d24877b558acdf8c380e1d8e6a2255cd726c
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2024-08-28 23:09:06 +0300

    update_28/08/2024

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,13 +8,20 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
   const [isActiveMenuLang, setIsActiveMenuLang] = useState(false);
+  const [isActiveMenuMobileLang, setIsActiveMenuMobileLang] = useState(false);
   const [isActiveMobileMenu, setIsActiveMobileMenu] = useState(false);
 
   const toggleLangMenu = () => {
     setIsActiveMenuLang(!isActiveMenuLang);
   };
 
+  const toggleLangMenuMobile = () => {
+    setIsActiveMenuMobileLang(!isActiveMenuMobileLang);
+  };
+
   const toggleMobileMenu = () => {
+    setIsActiveMenuLang(false);
+    setIsActiveMenuMobileLang(false);
     setIsActiveMobileMenu(!isActiveMobileMenu);
   };
 
@@ -33,9 +40,9 @@ const Header = () => {
     </header>
     <div className={isActiveMobileMenu ? (scss.mobile_menu_active) : (scss.mobile_menu)}>
         <div className={scss.mobile_menu_buttons}>
-          <button className={scss.mobile_lang_switcher} onClick={toggleLangMenu}>
+          <button className={scss.mobile_lang_switcher} onClick={toggleLangMenuMobile}>
               <span className={scss.lang}>UK</span>
-              <Polygon className={isActiveMenuLang ? (scss.polygon_reverse) : (scss.polygon)}/>
+              <Polygon className={isActiveMenuMobileLang ? (scss.polygon_reverse) : (scss.polygon)}/>
           </button>
           <button className={scss.close_menu} onClick={toggleMobileMenu}><IoMdClose className={scss.close_menu_icon}/></button>
         </div>
@@ -51,6 +58,10 @@ const Header = () => {
             <HandshakeWhite/>
             <span>Підтримати проєкт</span>
         </Link>
+        <div className={isActiveMenuMobileLang ? (scss.lang_mobile_active) : (scss.lang_mobile_menu)}> 
+            <span className={scss.lang}>EN</span>
+            <span className={scss.lang}>DE</span>
+        </div>
     </div>
     </>
         
