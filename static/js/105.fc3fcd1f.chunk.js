0a7fbf803c4d98710cd5a3dc4957b6b4f0a30ea7
Bachynckyi <93881817+Bachynckyi@users.noreply.github.com>
2024-12-16 21:48:18 +0000

    Deploying to gh-pages from @ Bachynckyi/aurum@a60a2bac18dc3d0b8d8454dc8c3f282711cb6a46 🚀

--- static/js/105.fc3fcd1f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[105],{9105:function(e,a,s){s.r(a),s.d(a,{default:function(){return i}});var t="ReviewsPage_container__ZLegX",n="ReviewsPage_content_wrapper__iA8ed",r="ReviewsPage_title__XPDI9",c=s(184),i=function(){return(0,c.jsx)("div",{className:t,children:(0,c.jsxs)("div",{className:n,children:[(0,c.jsx)("h1",{className:r,children:"\u0412\u0456\u0434\u0433\u0443\u043a\u0438"}),(0,c.jsx)("ul",{})]})})}}}]);
-//# sourceMappingURL=105.fc3fcd1f.chunk.js.map
