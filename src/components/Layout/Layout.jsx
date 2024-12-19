88b75586c83308fbdfd0c659d8830161d32945f5
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2024-12-19 23:02:34 +0200

    update_19/12/2024-2

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,20 @@
 import Footer from "components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import scss from "./Layout.module.scss";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const Layout = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      document.documentElement.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    }, [pathname]);
+    
     return (
         <div className={scss.container_page}>
             <div className={scss.main_container}>
