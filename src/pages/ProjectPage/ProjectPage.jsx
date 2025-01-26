28a418972c97db76898065ae42bce7da2f16b3a5
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2025-01-26 17:15:37 +0200

    update_26/01/2025-2

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,5 +1,15 @@
 import scss from './ProjectPage.module.scss';
 import { useTranslation } from 'react-i18next';
+import image1 from "../../images/project/project1.jpg";
+import image2 from "../../images/project/project2.jpg";
+import image3 from "../../images/project/project3.jpg";
+import image4 from "../../images/project/project4.jpg";
+import image5 from "../../images/project/project5.jpg";
+import image6 from "../../images/project/project6.jpg";
+import image7 from "../../images/project/project7.jpg";
+import image8 from "../../images/project/project8.jpg";
+import image9 from "../../images/project/project9.jpg";
+
 
 const ProjectPage = () => {
   const { t } = useTranslation();
@@ -7,27 +17,65 @@ const ProjectPage = () => {
   return (
     <div className={scss.container}>
           <div className={scss.content_wrapper}>
-          <h1 className={scss.title}>{t("Project_title")}</h1>
-          <div>
-            <p className={scss.text}>{t("Project_text1")}</p>
-            <p className={scss.text}>{t("Project_text2")}</p>
-            <ul className={scss.list}>{t("Project_list1_title")}
-                <li>{t("Project_list1_item1")}</li>
-                <li>{t("Project_list1_item2")}</li>
-                <li>{t("Project_list1_item3")}</li>
-                <li>{t("Project_list1_item4")}</li>
-            </ul>
-            <p className={scss.text}>{t("Project_text3")}</p>
-            <ul className={scss.list}>{t("Project_list2_title")}
-                <li>{t("Project_list2_item1")}</li>
-                <li>{t("Project_list2_item2")}</li>
-                <li>{t("Project_list2_item3")}</li>
-                <li>{t("Project_list2_item4")}</li>
-                <li>{t("Project_list2_item5")}</li>
-                <li>{t("Project_list2_item6")}</li>
-            </ul>
-            <p className={scss.text}>{t("Project_text4")}</p>
-          </div>
+            <h1 className={scss.title}>{t("Project_title")}</h1>
+            <div className={scss.intro_wrapper}>
+                <p className={scss.text}>{t("Project_text1")}</p>
+                <div className={scss.images_wrapper1}>
+                    <img src={image3} alt="image3" className={scss.image}/>
+                    <div className={scss.images_wrapper2}>
+                        <img src={image1} alt="image1" className={scss.image}/>
+                        <img src={image2} alt="image2" className={scss.image}/>
+                    </div>
+                </div>
+            </div>
+            <div className={scss.parzival_wrapper}>
+                <p className={scss.text}>{t("Project_text2")}</p>
+                <div className={scss.parzival_wrapper1}>
+                    <img src={image4} alt="image4" className={scss.image}/>
+                    <div className={scss.parzival_wrapper2}>
+                        <p className={scss.text}>{t("Project_list1_title")}</p>
+                        <ul className={scss.list}>
+                            <li>{t("Project_list1_item1")}</li>
+                            <li>{t("Project_list1_item2")}</li>
+                            <li>{t("Project_list1_item3")}</li>
+                            <li>{t("Project_list1_item4")}</li>
+                        </ul>
+                    </div>     
+                </div>
+                <div>
+                    <div className={scss.images_wrapper1}>
+                        <div className={scss.images_wrapper2}>
+                            <img src={image5} alt="image5" className={scss.image}/>
+                            <img src={image6} alt="image6" className={scss.image}/>
+                        </div>
+                        <img src={image7} alt="image7" className={scss.image}/>
+                </div>
+                </div>
+            </div>
+
+
+
+            <div className={scss.finish_wrapper}>
+                <div>
+                    <p className={scss.text}>{t("Project_text3")}</p>
+                    <p className={scss.text}>{t("Project_text4")}</p>
+                </div>
+                <div>
+                    <p className={scss.text}>{t("Project_list2_title")}</p>
+                    <ul className={scss.list}>
+                        <li>{t("Project_list2_item1")}</li>
+                        <li>{t("Project_list2_item2")}</li>
+                        <li>{t("Project_list2_item3")}</li>
+                        <li>{t("Project_list2_item4")}</li>
+                        <li>{t("Project_list2_item5")}</li>
+                        <li>{t("Project_list2_item6")}</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={scss.images_wrapper1}>
+                <img src={image8} alt="image8" className={scss.image}/>
+                <img src={image9} alt="image9" className={scss.image}/>
+            </div>
         </div>
     </div>
         
