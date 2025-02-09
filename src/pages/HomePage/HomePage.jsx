d16aeef656fd70f8fe17ba725516e497d2d00d66
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2025-02-09 20:58:37 +0200

    update_09/02/2025-16

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -56,7 +56,10 @@ const HomePage = () => {
             <meta property="og:description" content={t("Home_meta_description")}/>
             <meta property="og:url" content="https://www.clinic-aurum.com/" />
             <meta property="og:type" content="website"/>
-            <meta property="og:image" content="https://www.clinic-aurum.com/public/favicon.png"/>
+            <meta property="og:image" content="https://bachynckyi.github.io/aurum/favicon.png"/>
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
+            <meta name="twitter:card" content="summary_large_image" />
         </Helmet>
         <Header/>
         <motion.div
